95cdcb92ffb7f8633a0160b8f4e0b32b5397efad
emines0 <emin.sefa1@gmail.com>
2024-12-29 16:22:18 +0100

    added functions for input validation onlyNumbers, onlyLetters etc.

--- dev256593/global/sys_script_include/Ajax_EUtils.script.js
+++ dev256593/global/sys_script_include/Ajax_EUtils.script.js
@@ -4,8 +4,9 @@ Ajax_EUtils.prototype = Object.extendsObject(AbstractAjaxProcessor, {
     return true;
   },
 
-  isDateInPast: function () {
+  whichTimeline: function () {
     /**
+     * @function whichTimeline - Check if input date is in the past, future or current
      * @param sysparm_picked_date - Date you want to check in client script
      * @var inputDate - Formated date to currentDate format
      * @var inputDateArr - Array of inputDate
@@ -46,6 +47,146 @@ Ajax_EUtils.prototype = Object.extendsObject(AbstractAjaxProcessor, {
     } else {
       result = "current";
       return result;
+    }
+  },
+
+  isThereOnlyNumbers: function () {
+    /**
+     * @function isThereOnlyNumbers - Check if input string contains only numbers
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString contains only numbers, false if inputString contains anything else
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/^\d+$/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify;
+    }
+  },
+
+  isThereOnlyLetters: function () {
+    /**
+     * @function isThereOnlyLetters - Check if input string contains only letters
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString contains only letters, false if inputString contains anything else
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/^[a-zA-Z]+$/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify(result);
+    }
+  },
+
+  isThereOnlyLettersAndNumbers: function () {
+    /**
+     * @function isThereOnlyLettersAndNumbers - Check if input string contains only letters and numbers
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString contains only letters and numbers, false if inputString contains anything else
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/^[a-zA-Z0-9]+$/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify(result);
+    }
+  },
+
+  isTherSpace: function () {
+    /**
+     * @function isTherSpace - Check if input string contains space
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString contains space, false if inputString does not contain space
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/\s/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify(result);
+    }
+  },
+
+  isThereOnlyLettersAndSpace: function () {
+    /**
+     * @function isThereOnlyLettersAndSpace - Check if input string contains only letters and space
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString contains only letters and space, false if inputString contains anything else
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/^[a-zA-Z\s]+$/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify(result);
+    }
+  },
+
+  isThereOnlyNumbersAndSpace: function () {
+    /**
+     * @function isThereOnlyNumbersAndSpace - Check if input string contains only numbers and space
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString contains only numbers and space, false if inputString contains anything else
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/^[0-9\s]+$/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify(result);
+    }
+  },
+
+  isThereEmail: function () {
+    /**
+     * @function isThereEmail - Check if input string is email
+     * @param sysparm_input_string - String you want to check in client script
+     * @var inputString - String you want to check
+     * @returns - JSON.stringify(result) - true if inputString is email, false if inputString is not email
+     */
+
+    var inputString = this.getParameter("sysparm_input_string").toString();
+    var result = false;
+
+    if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(inputString)) {
+      result = true;
+      return JSON.stringify(result);
+    } else {
+      result = false;
+      return JSON.stringify(result);
     }
   },
 
